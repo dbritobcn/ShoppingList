a2387720621ef15295fbce01cfa6704c45d60b11
David Brito <dbritof@gmail.com>
2021-05-06 14:42:28 +0200

    Assets

--- Setup.js
+++ Setup.js
@@ -3,14 +3,24 @@ import App from './App';
 import firebase from '@react-native-firebase/app';
 import database from '@react-native-firebase/database';
 
+const {
+  REACT_APP_PROJECT_ID,
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_DATABASE_URL,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENDER_ID,
+  REACT_APP_APP_ID,
+} = process.env;
+
 const firebaseConfig = {
-  projectId: 'roomies-914c4',
-  apiKey: 'AIzaSyAszmhdJm0uAjhcD1qGmK_43pAokAri7dU',
-  authDomain: 'roomies-914c4.firebaseapp.com',
-  databaseURL: 'https://roomies-914c4.firebaseio.com',
-  storageBucket: 'roomies-914c4.appspot.com',
-  messagingSenderId: '1068410044812',
-  appId: '1:1068410044812:android:f6651b5465f4b2a6e456a9',
+  projectId: REACT_APP_PROJECT_ID,
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  databaseURL: REACT_APP_DATABASE_URL,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
 };
 
 if (!firebase.apps.length) {
